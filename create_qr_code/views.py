1beb1004c6dc9a49a0db2a3b40ec1d7ddd2c20f5
dimachep1408 <dmitriychep2011@gmail.com>
2025-02-02 09:16:51 +0200

    created function to delete qr-code image in files

--- create_qr_code/views.py
+++ create_qr_code/views.py
@@ -31,12 +31,12 @@ def render_create_qr_code_page(request):
         qr_code.make()
         
         try:
-            img = qr_code.make_image(fill_color = qr_code_color, back_color = qr_code_back_color, module_drawer=CircleModuleDrawer())
+            img = qr_code.make_image(fill_color = qr_code_color, back_color = qr_code_back_color)
             img.save(os.path.abspath(__file__ + "/../../media/qr_codes/demo/qrcode.png"))
         except ValueError:
             qr_code_back_color = "white"
             qr_code_color = "black"
-            img = qr_code.make_image(fill_color = qr_code_color, back_color = qr_code_back_color, module_drawer=CircleModuleDrawer(resample_method=None))
+            img = qr_code.make_image(fill_color = qr_code_color, back_color = qr_code_back_color)
             img.save(os.path.abspath(__file__ + "/../../media/qr_codes/demo/qrcode.png"))
 
         if "save" in request.POST:
--- my_qr_codes/views.py
+++ my_qr_codes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from create_qr_code.models import QrCodes
+import os
+
 
 # Create your views here.
 
@@ -21,11 +23,13 @@ def render_my_qr_codes_page(request):
     if request.method == "POST":
         deleleted_qr_code = request.POST.get("delete")
 
-        print(deleleted_qr_code)
+        qr_code_name = QrCodes.objects.get(id = deleleted_qr_code).name
 
         if deleleted_qr_code != None:
 
             QrCodes.objects.get(id = deleleted_qr_code).delete()
+            os.remove(os.path.abspath(__file__ + f"/../../media/qr_codes/image/{username}_{qr_code_name}.png"))
+
             return redirect("/my_qr_codes_page/")
 
 
