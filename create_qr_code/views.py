27e03407d1224c0d34adb05d8a8be4dc73aaab1d
dimachep1408 <dmitriychep2011@gmail.com>
2025-02-08 08:22:25 +0200

    Created validation for save qr-code, if qrcode with this name already saved QR-code will not be saved, creating the ability to add a frame using checkbox

--- create_qr_code/views.py
+++ create_qr_code/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db import IntegrityError
 from .models import QrCodes
+from PIL import Image, ImageDraw
 from registration.models import Profile
 from django.contrib.auth.models import User
 from qrcode.image.styledpil import StyledPilImage
@@ -40,16 +41,17 @@ def render_create_qr_code_page(request):
         dots_form = request.POST.get("dots")
         eye_form = request.POST.get("eye")
 
+        frame_around =  request.POST.get("circle")
+
+        print("\n\n\n\n\n",frame_around, "\n\n\n\n\n\n\n")
+
         if qr_code_name == "":
             return redirect("/create_qr_code_page/")
-        
-        print(qr_code_color)        
+               
 
         qr_code = qrcode.QRCode()
         qr_code.add_data(qr_code_url)
         qr_code.make()
-        
-        print(logotype)
 
 
         eye_drawer_module = SquareModuleDrawer()
@@ -66,9 +68,24 @@ def render_create_qr_code_page(request):
         img = qr_code.make_image(fill_color = qr_code_color, back_color = qr_code_back_color, module_drawer= dots_drawer_module, eye_drawer=eye_drawer_module, image_factory=StyledPilImage)
 
 
-        img.save(os.path.abspath(__file__ + f"/../../media/qr_codes/demo/{username}_qrcode.png"))
+
+        if frame_around == "on":
+            draw = ImageDraw.Draw(img)
+
+            draw.ellipse(
+                (0, 0, img.size[1], img.size[1]),
+                fill = None,
+                outline ='black',
+                width=5
+            )
+
 
         qr_codes = QrCodes.objects.filter(user_id = username.id)
+
+        qrcode_names = []
+
+        for qr_code in qr_codes:
+            qrcode_names.append(qr_code.name)
 
         print(len(qr_codes) + 1)
 
@@ -81,15 +98,19 @@ def render_create_qr_code_page(request):
         elif user_now.subscribe == "pro":
             maximum_qr_codes = 100
 
-        if len(qr_codes) < maximum_qr_codes:
+        if qr_code_name not in qrcode_names:
 
-            if "save" in request.POST:
-                # try:
-                QrCodes.objects.create(name = qr_code_name, image = f"/../../media/qr_codes/image/{username}/{qr_code_name}.png", user = username)
-                img.save(os.path.abspath(__file__ + f"/../../media/qr_codes/image/{username}/{qr_code_name}.png"))
-                # except:
-                #     error = "this name already used"
-            return redirect("/create_qr_code_page/")
+            if len(qr_codes) < maximum_qr_codes:
+
+                if "save" in request.POST:
+                    # try:
+                    img.save(os.path.abspath(__file__ + f"/../../media/qr_codes/demo/{username}_qrcode.png"))
+                    QrCodes.objects.create(name = qr_code_name, image = f"/../../media/qr_codes/image/{username}/{qr_code_name}.png", user = username)
+                    img.save(os.path.abspath(__file__ + f"/../../media/qr_codes/image/{username}/{qr_code_name}.png"))
+                    # except:
+                    #     error = "this name already used"
+        return redirect("/create_qr_code_page/")
 
 
     return render(request, "create_qr_code.html", context = {"username" : username, })
+
